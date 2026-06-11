13479c01391dcb104d779452eda09a00b6888276
davidmunoznovoa <dmunoz@gisce.net>
2026-06-11 13:53:08 +0200

    Add tqdm in insert_batch function

--- cchloader/backends/base.py
+++ cchloader/backends/base.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from tqdm import tqdm
 import traceback
 
 
@@ -23,7 +24,7 @@ class BaseBackend(object):
     def insert_batch(self, documents):
         """Insert documents in batch. Returns list of error tracebacks."""
         errors = []
-        for document in documents:
+        for document in tqdm(documents):
             try:
                 self.insert(document)
             except Exception as _:
